06efa7b89d348462fa34ee4420de42066eba022e
Sohailbasha <shlbsh94@gmail.com>
2017-02-14 16:20:35 -0700

    tab bar created

---
+++ CustomTabBarViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTabBarViewController.swift
+//  Habit Tracker
+//
+//  Created by Ilias Basha on 2/14/17.
+//  Copyright © 2017 Nicholas Ellis. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarViewController: UITabBarController {
+
+    @IBOutlet var addButtonTabView: UIView!
+    
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let frame = CGRect(x: view.center.x - 35, y: view.frame.height - 70, width: 70, height: 70)
+        addButtonTabView.frame = frame
+        addButtonTabView.layer.cornerRadius = 0.5 * addButtonTabView.bounds.size.width
+        addButtonTabView.clipsToBounds = true
+        view.addSubview(addButtonTabView)
+        
+    }
+
+   
+    
+
+
+    
+}
---
+++ Habit Tracker/AddHabitViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddHabitViewController.swift
+//  Habit Tracker
+//
+//  Created by Ilias Basha on 2/14/17.
+//  Copyright © 2017 Nicholas Ellis. All rights reserved.
+//
+
+import UIKit
+
+class AddHabitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    
+    
+    
+    
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var habitNameTextField: UITextField!
+    @IBOutlet var timeOfDayLabel: UILabel!
+    @IBOutlet var timeDetailLabel: UILabel!
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func dismissButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func leftToDButtonTapped(_ sender: Any) {
+    }
+    
+    
+}
