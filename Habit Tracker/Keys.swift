429d50c47cc7c712c2a15f1d8b1043b2128c5e4d
Keaton Harward <keatonharward@gmail.com>
2017-02-15 10:37:06 -0700

    added asset images and keys for UIColor

---
+++ Habit Tracker/Keys.swift
@@ -0,0 +1,26 @@
+//
+//  Keys.swift
+//  Habit Tracker
+//
+//  Created by Keaton Harward on 2/15/17.
+//  Copyright © 2017 Nicholas Ellis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Keys {
+    
+    static let shared = Keys()
+    
+    let iconTeal = UIColor(colorLiteralRed: 23 / 255, green: 181 / 255, blue: 160 / 255, alpha: 1)
+    let iconPink = UIColor(colorLiteralRed: 181 / 255, green: 23 / 255, blue: 138 / 255, alpha: 1)
+    let iconPurple = UIColor(colorLiteralRed: 110 / 255, green: 23 / 255, blue: 181 / 255, alpha: 1)
+    let iconGreen = UIColor(colorLiteralRed: 23 / 255, green: 181 / 255, blue: 28 / 255, alpha: 1)
+    let iconRed = UIColor(colorLiteralRed: 181 / 255, green: 23 / 255, blue: 23 / 255, alpha: 1)
+    let iconYellow = UIColor(colorLiteralRed: 178 / 255, green: 181 / 255, blue: 23 / 255, alpha: 1)
+    let iconBlue = UIColor(colorLiteralRed: 23 / 255, green: 58 / 255, blue: 181 / 255, alpha: 1)
+    
+    let cellBackground = UIColor(colorLiteralRed: 26 / 255, green: 26 / 255, blue: 26 / 255, alpha: 1)
+    let background = UIColor(colorLiteralRed: 65 / 255, green: 65 / 255, blue: 65 / 255, alpha: 1)
+}
