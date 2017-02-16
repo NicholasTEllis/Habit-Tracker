4e385c3c3bffec3b8adbf8f9ed39a92a75192617
NicholasTEllis <e.nicholas72@yahoo.com>
2017-02-16 10:26:20 -0700

    fixedAddHabit

--- Habit Tracker/AddHabitViewController.swift
+++ Habit Tracker/AddHabitViewController.swift
@@ -22,6 +22,7 @@ class AddHabitViewController: UIViewController {
         self.view.addGestureRecognizer(swipeRight)
         swipeRight.addTarget(self, action: #selector(respondToSwipeGesture(_:)))
 
+        
         let swipeLeft = UISwipeGestureRecognizer()
         swipeLeft.direction = .left
         self.view.addGestureRecognizer(swipeLeft)
@@ -56,17 +57,13 @@ class AddHabitViewController: UIViewController {
     func selectTime(index: Int) {
         switch index {
         case 0:
-            timeOfDayLabel.text = "Indefinitely"
+            timeOfDayLabel.text = "Any"
         case 1:
             timeOfDayLabel.text = "Morning"
         case 2:
-            timeOfDayLabel.text = "Noon"
-        case 3:
             timeOfDayLabel.text = "Afternoon"
-        case 4:
+        case 3:
             timeOfDayLabel.text = "Evening"
-        case 5:
-            timeOfDayLabel.text = "Night"
         default:
             return
         }
@@ -106,7 +103,6 @@ class AddHabitViewController: UIViewController {
     // MARK: - Properties
     
     var index: Int = 0
-    
     var icon: UIImage?
     
     var imageIcon: [UIImage] {
@@ -161,6 +157,7 @@ extension AddHabitViewController: UICollectionViewDelegate, UICollectionViewData
         let icon = imageIcon[indexPath.row]
         self.icon = icon
     }
+
 }
 
 
