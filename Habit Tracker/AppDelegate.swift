ad3b92f790e99bd9358005231d2ab1fb78a2f600
NicholasTEllis <e.nicholas72@yahoo.com>
2017-02-19 13:35:45 -0700

    emptied app delegate

--- Habit Tracker/AppDelegate.swift
+++ Habit Tracker/AppDelegate.swift
@@ -14,20 +14,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (_, error) in
-            if let error = error {
-                NSLog("Error requesting authorization for notifications: \(error)")
-                return
-            }
-        }
-        UIApplication.shared.registerForRemoteNotifications()
-        
-        return true
     }
- 
-    func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
-        <#code#>
-    }
-}
 
