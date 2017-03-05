d23456e22b4096dd953b7577bcb745159ab4395a
zeuscaballero@icloud.com <zeuscaballero@icloud.com>
2017-03-05 16:30:58 -0700

    updating onboarding decriptions

--- Habit Tracker/OnboardingViewController.swift
+++ Habit Tracker/OnboardingViewController.swift
@@ -42,13 +42,21 @@ class OnboardingViewController: UIViewController, PaperOnboardingDataSource, Pap
         let titleFont = UIFont(name: "AvenirNext-Bold", size: 24)!
         let descriptionFont = UIFont(name: "AvenirNext-Regular", size: 18)!
         
+<<<<<<< Updated upstream
         return [(imageName: "group_4", title: "Welcome to 21habit", description: "Don't mind our mascot, Habit Rabbit. He's happier to see you than it seems.", iconName: "", color: backgroundColorOne, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
+=======
+        return [(imageName: "group_4", title: "Welcome to 21habit", description: "Meet habit rabbit. Think of him as your 21-day habit development coach. Do not disappoint him.", iconName: "", color: backgroundColorOne, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
+>>>>>>> Stashed changes
                 
-            (imageName: "OnboardingHabits", title: "How it works", description: "We're sure you've heard that it takes 21 days to form a habit. 21habit is the app that encourages to create new ones for yourself while monitoring your progress for 21 days. It could be anything from fitness-related goals to quitting cigarettes cold turkey.", iconName: "", color: backgroundColorTwo, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
+            (imageName: "OnboardingHabits", title: "How it works", description: "We're sure you've heard that it takes 21 days to form a habit. 21habit is the app that encourages you to create new ones for yourself while monitoring your progress from start to finish. It could be anything from fitness-related goals to quitting cigarettes cold turkey.", iconName: "", color: backgroundColorTwo, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
             
-            (imageName: "Onboarding3", title: "You get 3 strikes", description: "If you are not able to complete the habit we give you the option of either posting to social media or donating to a charity of your choice", iconName: "", color: backgroundColorThree, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
+            (imageName: "Onboarding3", title: "You'll get 3 strikes", description: "If you're not able to complete a habit, you'll have the option of either having your results posted to social media or donating to a charity of your choice.", iconName: "", color: backgroundColorThree, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
             
+<<<<<<< Updated upstream
             (imageName: "notification", title: "Please allow us to notify you", description: "Turn on your notifications so that we can remind you to complete your habit challenge for that day.", iconName: "", color: Keys.shared.iconColor8, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
+=======
+            (imageName: "notification", title: "21habit works best with notifications", description: "Please turn on your notifications so that rabbit can remind you to complete your daily habit.", iconName: "", color: backgroundColorFour, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
+>>>>>>> Stashed changes
                 ][index]
         
     }
