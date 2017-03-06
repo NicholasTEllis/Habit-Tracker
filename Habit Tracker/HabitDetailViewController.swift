e6e9c7a3408b05a78340670d7477982a5a87f237
zeuscaballero@icloud.com <zeuscaballero@icloud.com>
2017-03-06 09:23:33 -0700

    updated imageURL, added privacy policy

--- Habit Tracker/HabitDetailViewController.swift
+++ Habit Tracker/HabitDetailViewController.swift
@@ -17,15 +17,16 @@ class HabitDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         let content : FBSDKShareLinkContent = FBSDKShareLinkContent()
         content.contentURL = URL(string: "")
         content.contentTitle = "I started a new habit with 21habit"
         content.contentDescription = "Create better habits for yourself with 21habit. It's free on the app store!"
-        content.imageURL = URL(string: "https://s-media-cache-ak0.pinimg.com/564x/f8/88/eb/f888ebbf1c32893934ed29b7f90cc589.jpg")
+        content.imageURL = URL(string:"https://c1.staticflickr.com/1/746/33188446556_33c998f0a9_b.jpg")
         
         let button : FBSDKShareButton = FBSDKShareButton()
         button.shareContent = content
-        button.frame = CGRect(x: 0, y: 0, width: 100, height: 25) // move later
+        button.frame = CGRect(x: view.center.x, y: view.center.y, width: 100, height: 25) // move later
         self.view.addSubview(button)
         
         calendarCollectionView.delegate = self
@@ -312,10 +313,41 @@ extension HabitDetailViewController {
 }
 
 
-
-
-
-
+/*
+ Application Privacy Statement
+ 
+ This privacy statement (“Privacy Statement”) applies to the treatment of personally identifiable information submitted by, or otherwise obtained from, you in connection with the associated application (“21habit”). By using or otherwise accessing the Application, you acknowledge that you accept the practices and policies outlined in this Privacy Statement.
+ 
+WHAT PERSONAL INFORMATION DOES 21HABIT COLLECT?
+ 
+ We collect the following types of information from our users:
+ 
+ Personal Information You Provide to Us:
+ We may receive and store any information you submit to the Application (or otherwise authorize us to obtain – such as, from (for example) your Facebook account or Twitter account). The types of personal information collected may include your full name, email address, gender, username, and any other information necessary for us to provide the Application services.
+ 
+ 
+ 21habit takes all measures reasonably necessary to protect User Personal Information from unauthorized access, alteration, or destruction; maintain data accuracy; and help ensure the appropriate use of User Personal Information. We follow generally accepted industry standards to protect the personal information submitted to us, both during transmission and once we receive it.
+ 
+ 
+ 
+ WHAT PERSONAL INFORMATION CAN I ACCESS?
+ 
+ 21habit allows you to access the following information about you for the purpose of viewing, and in certain situations, updating that information. This list may change in the event the Application changes.
+ 
+ - Account and user profile information
+ - User e-mail address, if applicable
+ - Facebook profile information, if applicable
+ - User preferences
+ - Application specific data
+ 
+ 
+ CHANGES TO THIS PRIVACY STATEMENT.
+ 
+ 21habit may amend this Privacy Statement from time to time. Use of information we collect now is subject to the Privacy Statement in effect at the time such information is used. If we make changes in the way we use personal information, we will notify you by posting an announcement on our Site or sending you an email. Users are bound by any changes to the Privacy Statement when he or she uses or otherwise accesses the Application after such changes have been first posted.
+ 
+ QUESTIONS OR CONCERNS.
+ 
+ If you have any questions or concerns regarding privacy on our Website, please send us a detailed message at 21habitteam@gmail.com. We will make every effort to resolve your concerns.  */
 
 
 
