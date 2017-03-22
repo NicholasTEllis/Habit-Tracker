668703d59fa1f011a62145379aa014c50c1b5c44
NicholasTEllis <e.nicholas72@yahoo.com>
2017-03-21 21:29:02 -0400

    added seconds

--- Habit Tracker/HabitController.swift
+++ Habit Tracker/HabitController.swift
@@ -58,7 +58,7 @@ class HabitController {
         let content = UNMutableNotificationContent()
         content.title = "\(name)"
         content.body = "Finish Your Habit Today!"
-        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: fireDate as Date)
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: fireDate as Date)
         let dateTrigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         let request = UNNotificationRequest(identifier: HabitController.userNotificationIdentifier, content: content, trigger: dateTrigger)
         UNUserNotificationCenter.current().add(request) { (error) in
